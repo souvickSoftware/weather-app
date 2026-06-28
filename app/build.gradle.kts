90a603d4ffd3201b9d6a05043e56f6b1fb218c65
Souvick Biswas <souvick.biswas@etmoney.com>
2026-06-28 20:33:42 +0530

    Set up Hilt di

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -46,6 +49,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- app/src/main/java/com/souvick/weatherapp/WeatherApplication.kt
+++ app/src/main/java/com/souvick/weatherapp/WeatherApplication.kt
@@ -1,4 +1,8 @@
 package com.souvick.weatherapp
 
-class WeatherApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class WeatherApplication: Application()
